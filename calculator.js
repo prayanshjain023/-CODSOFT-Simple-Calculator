1acf2eb220cc7af1cc9358470280bde8d529441a
prayanshjain023 <prayanshjain023.com>
2024-01-03 12:06:29 +0530

    first commit

---
+++ calculator.js
@@ -0,0 +1,17 @@
+let display = document.getElementById('display');
+
+function operationPerform(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function calculateResult() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
